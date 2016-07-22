18b577df20a6e59c934a20a48d683d16e49cc8cc
Asher <asheransar@gamil.com>
2016-07-23 01:08:34 +0500

    This is The First Basic Code Which we have done in Our 4 Session

---
+++ Class_4/cal/src/cal/Cal.java
@@ -0,0 +1,48 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package cal;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author asher.ansari
+ */
+public class Cal {
+
+	/**
+	 * @param args the command line arguments
+	 */
+	public static void main(String[] args) {
+		// TODO code application logic here
+//		add objName= new add();
+//		String a ="kjncjnewv vznldjfvndf vonbd"
+//			+ "vnvnf"
+//			+ "nvosdnvklnmdfbi gjbgfbipj ";
+//		System.out.print(a);
+////		System.out.print(a);
+//		System.out.println("\n"+a);
+		
+		Scanner sc = new Scanner(System.in);
+		double a=0;
+		int b=0;
+		boolean check = false;
+		System.out.print("Enter The Value Of a = ");
+		a=sc.nextDouble();
+		System.out.print("\nEnter The Value Of b = ");
+		b=sc.nextInt();
+		System.out.println(" a ="+a);
+		System.out.println(" b ="+b);
+
+		System.out.println("chack = "+check);
+		check = sc.nextBoolean();
+		System.out.println("chack = "+check);
+		
+		
+//	objName.
+	}
+	
+}
